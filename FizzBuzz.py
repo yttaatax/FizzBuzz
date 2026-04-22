08c7f7130e49e93e54fa43b096b69d147751b654
yttaatax <yttaa.tax@gmail.com>
2026-04-22 22:15:54 +0700

    FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+a = int(input("Введите число:"))
+for num in range(1, a+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
